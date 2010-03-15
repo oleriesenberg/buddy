c81e3529a7d8f2bd88119567a76a7f8f4d8cc6c9
Ole Riesenberg <or@oleriesenberg.com>
2010-03-15 11:40:38 +0100

    No init.rb needed in Rails 3

--- init.rb
+++
@@ -1,1 +0,0 @@
-require 'fb_buddy'
--- lib/buddy.rb
+++ lib/buddy.rb
@@ -1,1 +1,33 @@
 require 'mini_fb'
+
+module Buddy
+  @buddy_config = {}
+
+  class << self
+    def load_configuration(yaml)
+      return false unless File.exist?(yaml)
+      @buddy_config = YAML.load(ERB.new(File.read(yaml)).result)[Rails.env]
+
+      buddy_config
+    end
+
+    def buddy_config
+      @buddy_config
+    end
+
+    attr_accessor :logger
+
+    def set_asset_host_to_callback_url
+      buddy_config[app]['set_asset_host_to_callback_url']
+    end
+
+    def timeout(app = 'default')
+      buddy_config[app]['timeout']
+    end
+  end
+end
+
+buddy_config = "#{Bundler.root}/config/buddy.yml"
+
+BUDDY = Buddy.load_configuration(buddy_config)
+
