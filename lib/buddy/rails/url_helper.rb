37204c55b770618bdafd4b0d6784422ba4028446
Klaus Breyer <bannii@gmail.com>
2010-04-14 17:54:51 +0200

    url helper fixed

--- lib/buddy/rails/url_helper.rb
+++ lib/buddy/rails/url_helper.rb
@@ -3,19 +3,21 @@ module Buddy
     module UrlHelper
       def url_for(options = {})
         options ||= {}
-	opts = case options
-	when Hash
+        opts = case options
+        when Hash
           options.merge!({ :only_path => true }) #unless options[:canvas] == false
-	else
-	  options
-	end
+        else
+          options
+        end
         url = super(opts)
 
-	if options.is_a?(Hash) && options[:canvas] == false
-	  "#{Buddy.current_config["callback_url"]}#{url}"
-	else
-	  "http://apps.facebook.com/#{Buddy.current_config["canvas_page_name"]}#{url}"
-	end
+        if url.include?("http://")  #todo: make it better
+          url
+        elsif options.is_a?(Hash) && options[:canvas] == false
+          "#{Buddy.current_config["callback_url"]}#{url}"
+        else
+          "http://apps.facebook.com/#{Buddy.current_config["canvas_page_name"]}#{url}"
+        end
       end
     end
   end
