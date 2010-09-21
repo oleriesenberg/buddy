5930072eecdd0d6aedc64ae38421a1797591d389
Ole Riesenberg <or@oleriesenberg.com>
2010-09-21 15:36:10 +0200

    remove useless stuff from Railtie

--- lib/buddy/railtie.rb
+++ lib/buddy/railtie.rb
@@ -5,14 +5,8 @@ require 'rails'
 module Buddy
   class Railtie < ::Rails::Railtie
     initializer "buddy.configure_rails_initialization" do |app|
-      app.config.middleware.insert_before(ActionDispatch::RemoteIp, ::Rack::Facebook::RemoteIp)
       app.config.middleware.insert_before(ActionDispatch::ParamsParser, ::Rack::Facebook::ParamsParser)
-      app.config.action_controller.asset_host = Buddy.buddy_config['default']['callback_url']
-
       ActionView::Helpers::UrlHelper.send(:include, Buddy::Rails::UrlHelper)
-
-      Mime::Type.register "text/html", :fbml
-      Mime::Type.register "text/javascript", :fbjs
       Buddy.logger = ::Rails.logger
     end
   end
