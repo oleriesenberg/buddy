84418ff5be8668a94d0f063aa6f730306c851719
Ole Riesenberg <or@oleriesenberg.com>
2011-02-17 18:45:05 +0100

    removed RemoteIp middleware class and updated comments in middleware module

--- lib/rack/facebook.rb
+++ lib/rack/facebook.rb
@@ -1,41 +1,11 @@
 module Rack
-  # This Rack middleware checks the signature of Facebook params, and
-  # converts them to Ruby objects when appropiate. Also, it converts
-  # the request method from the Facebook POST to the original HTTP
-  # method used by the client.
+  # This Rack middleware checks the signed_request, and
+  # appends the payload to Rails params.
   #
   # If the signature is wrong, it returns a "400 Invalid Facebook Signature".
   #
-  # Optionally, it can take a block that receives the Rack environment
-  # and returns a value that evaluates to true when we want the middleware to
-  # be executed for the specific request.
-  #
-  # == Usage
-  #
-  # In your config.ru:
-  #
-  #   require 'rack/facebook'
-  #   use Rack::Facebook, "my_facebook_secret_key"
-  #
-  # Using a block condition:
-  #
-  #   use Rack::Facebook, "my_facebook_secret_key" do |env|
-  #     env['REQUEST_URI'] =~ /^\/facebook_only/
-  #   end
-  #
 
   module Facebook
-    class RemoteIp
-      def initialize(app)
-        @app = app
-      end
-
-      def call(env)
-        env['REMOTE_ADDR'] = env['X-FB-USER-REMOTE-ADDR'] if env['X-FB-USER-REMOTE-ADDR']
-        @app.call(env)
-      end
-    end
-
     class ParamsParser
       def initialize(app, &condition)
         @app = app
