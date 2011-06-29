52fe1fbcdb37d5f89a9e8033405848b9dff1d33a
Ole Riesenberg <or@oleriesenberg.com>
2011-06-29 20:30:24 +0200

    Use new URL for FB OAuth dialog

--- lib/buddy/session.rb
+++ lib/buddy/session.rb
@@ -43,7 +43,7 @@ module Buddy
     end
 
     def install_url(options = {})
-      "http://www.facebook.com/connect/uiserver.php?app_id=#{Buddy.current_config['app_id']}&next=#{options[:next]}&display=page&canvas=1&legacy_return=1&method=permissions.request#{"&perms="+Buddy.current_config['perms'] if Buddy.current_config['perms']}"
+      "https://www.facebook.com/dialog/oauth?client_id=#{Buddy.current_config['app_id']}&redirect_uri=#{options[:next]}#{"&scope="+Buddy.current_config['perms'] if Buddy.current_config['perms']}"
     end
 
     def initialize(app_id, secret_key)
