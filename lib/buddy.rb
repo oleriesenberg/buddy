29984303587f64d0566fdf7587994aaa4662d940
Ole Riesenberg <or@oleriesenberg.com>
2011-07-01 18:43:43 +0200

    removed multi app support and some unused config accessors

--- lib/buddy.rb
+++ lib/buddy.rb
@@ -21,37 +21,22 @@ module Buddy
     def load_configuration(yaml)
       return false unless File.exist?(yaml)
       @buddy_config = YAML.load(ERB.new(File.read(yaml)).result)[::Rails.env]
-      self.current_config = buddy_config['default']
 
-      buddy_config
+      unless self.config['default'].nil?
+        ActiveSupport::Deprecation.warn('Support for multiple apps has been removed and your config format is deprecated. Please remove your app keys (or just the "default" key)')
+        self.config = self.config['default']
+      end
+      self.config
     end
 
-    def buddy_config
+    def config
       @buddy_config
     end
+    alias :buddy_config :config
+    alias :current_config :config
 
-    def set_asset_host_to_callback_url
-      buddy_config[app]['set_asset_host_to_callback_url']
-    end
-
-    def timeout(app = 'default')
-      buddy_config[app]['timeout']
-    end
-
-    def use_application(api_key)
-      buddy_config.each do |c|
-        if c[1]["api_key"] == api_key
-          return self.current_config = c[1]
-        end
-      end
-    end
-
-    def current_config
-      Thread.current['current_buddy_config']
-    end
-
-    def current_config=(config)
-      Thread.current['current_buddy_config'] = config
+    def config=(c)
+      @buddy_config = c
     end
   end
 end
