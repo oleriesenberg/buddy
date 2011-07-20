282fe656d546538e6e78d8d8e960ccc759656175
Ole Riesenberg <or@oleriesenberg.com>
2011-07-20 13:48:08 +0200

    Make sure that expires is integer

--- lib/buddy/middleware.rb
+++ lib/buddy/middleware.rb
@@ -97,7 +97,7 @@ module Buddy
         expires      = payload['expires']
 
         @request.session['facebook_session'] = Buddy::Session.create(Buddy.current_config['app_id'], Buddy.current_config['secret'])
-        @request.session['facebook_session'].secure!(uid.to_i, access_token, expires)
+        @request.session['facebook_session'].secure!(uid.to_i, access_token, expires.to_i)
       end
     end
   end
