9c74fca6a77254678b9ee380e478a34944246eb8
Paul Asmuth <paul@paulasmuth.com>
2011-10-05 11:07:18 +0200

    allow strings to be passed into url_for, but still add apps.fb.com unless it's an absolute url

--- lib/buddy/rails/helpers.rb
+++ lib/buddy/rails/helpers.rb
@@ -2,9 +2,7 @@ module Buddy
   module Helpers
     module UrlFor
       def url_for(options = nil)
-        return options unless options.is_a?(Hash)
-        only_path = options[:only_path] || false
-        canvas = options.delete(:canvas)
+        canvas = options.is_a?(Hash) && options.delete(:canvas)
         options.merge!({ :only_path => true }) if options.is_a?(Hash) && canvas != false
 
         url = super(options)
@@ -12,6 +10,7 @@ module Buddy
         if url[0..6] == 'http://' or url[0..7] == 'https://'
           url
         elsif options.is_a?(Hash) && canvas == false
+          only_path = options[:only_path] || false
           base = only_path ? '' : Buddy.current_config["callback_url"]
           base + url
         else
